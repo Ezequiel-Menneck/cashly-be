6555066e1db17af25d1dbb88b6de272fb05207ca
menneck <zeeeee.peeeeetryyy@gmail.com>
2025-01-09 12:57:13 -0300

    fix: Fixing type of transactionDate

--- src/main/java/org/cashly/User/Transactions/DTOs/CreateTransactionDTO.java
+++ src/main/java/org/cashly/User/Transactions/DTOs/CreateTransactionDTO.java
@@ -3,12 +3,11 @@ package org.cashly.User.Transactions.DTOs;
 import org.cashly.User.Transactions.TransactionType;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public record CreateTransactionDTO(
         String identifier,
         BigDecimal amount,
-        LocalDateTime transactionDate,
+        String transactionDate,
         String description,
         TransactionType type,
         String categoryName
